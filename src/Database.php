a6cba6e04b2fccbe4f957665fad6f2d4abb43b15
Eroslanov <166691699+Eroslanov@users.noreply.github.com>
2024-12-11 22:54:01 +0300

    Update Database.php

--- src/Database.php
+++ src/Database.php
@@ -13,26 +13,26 @@ class Database
     {
         $dsn = "mysql:host={$config['host']};dbname={$config['dbname']};charset={$config['charset']}";
         $options = [
-            PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
             PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-            PDO::ATTR_EMULATE_PREPARES   => false,
+            PDO::ATTR_EMULATE_PREPARES => false,
         ];
 
         try {
             $this->pdo = new PDO($dsn, $config['username'], $config['password'], $options);
         } catch (PDOException $e) {
-            throw new PDOException($e->getMessage(), (int)$e->getCode());
+            throw new PDOException($e->getMessage(), (int) $e->getCode());
         }
     }
 
-    public function query($sql, $params = [])
+    public function query(string $sql, array $params = []): PDOStatement
     {
         $stmt = $this->pdo->prepare($sql);
         $stmt->execute($params);
         return $stmt;
     }
 
-    public function lastInsertId()
+    public function lastInsertId(): int
     {
         return $this->pdo->lastInsertId();
     }
